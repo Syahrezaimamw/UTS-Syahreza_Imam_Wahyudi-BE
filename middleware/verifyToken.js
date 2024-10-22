fddf47c5038f680e696655e0a08fcc6673d8ee7a
Syahrezaimamw <syahrezaimamwahyudi976@gmail.com>
2024-10-22 10:20:56 +0700

    fiks response

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -6,7 +6,7 @@ export const verifyToken = async (req, res, next) => {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1]
     if (!token) {
-        response(403, res, 'Not Tokken')
+        response(403, res, 'Not Token')
     } else {
         jwt.verify(token, process.env.ACCESS_TOKEN_SECRET,async (err, decode) => {
             if (err) response(403, res, 'the token is incorrect or has expired') //forbiden 
